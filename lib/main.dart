d51c29dae7ddb4527ecd01d3448b3342ea6ee99e
TatinaOsmon <tatinaosmonova@gmail.com>
2024-02-09 16:50:42 +0800

    gfgd

--- lib/main.dart
+++ lib/main.dart
@@ -340,6 +340,8 @@ class _NavBarPageState extends State<NavBarPage> {
                         : FlutterFlowTheme.of(context).secondaryText,
                     size: 35.0,
                   ),
+                  //nfjnewf
+                  Text('data'),
                   Text(
                     '個人資料',
                     overflow: TextOverflow.ellipsis,
